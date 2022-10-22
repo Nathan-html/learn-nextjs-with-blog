122279b354c932d33e96fd409d75869952897794
Nathan Flacher <nathan.flacher.pro@gmail.com>
2022-10-22 21:21:15 +0200

    Image Component and Image Optimization

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,19 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function FirstPost() {
     return <div className="container">
-        <h1>First Post</h1>
-        <Link href="../">Back to home</Link>
+        <main>
+            <h1 className="title">
+                First Post
+            </h1>
+            <Image
+                src="/images/rocket-league-dominus.png"
+                height={144}
+                width={200}
+                alt="Rocket League dominus"
+            />
+            <p><Link href="../">Back to home</Link></p>
+        </main>
     </div>;
 }
